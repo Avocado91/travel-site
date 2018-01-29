37724f8f7f360345dd9e56a638483442a5c34315
T.J <t.ruggles@live.ca>
2018-01-29 02:01:59 -0500

    set up javascript functionality for menu icon on small screens

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,19 +1,2 @@
-const $ = require('jquery');
-import Person from './modules/Person';
-
-class Adult extends Person{
-  payTaxes() {
-    console.log(this.name + ' now owes $0 in taxes.')
-  }
-}
-
-const john = new Person('John Doe', 'blue');
-const jane = new Adult('Jane Smith', 'orange');
-
-alert('bleh');
-
-john.greet();
-jane.greet();
-jane.payTaxes();
-
-$('h1').remove();
+import MobileMenu from './modules/MobileMenu';
+const mobileMenu = new MobileMenu();
