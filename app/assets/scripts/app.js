e433de56eb223ef0ac2934134b6db44918306d8f
T.J <t.ruggles@live.ca>
2018-01-28 20:57:42 -0500

    setting up babel

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,12 +1,19 @@
 const $ = require('jquery');
-const Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person{
+  payTaxes() {
+    console.log(this.name + ' now owes $0 in taxes.')
+  }
+}
 
 const john = new Person('John Doe', 'blue');
-const jane = new Person('Jane Smith', 'green');
+const jane = new Adult('Jane Smith', 'orange');
 
 alert('bleh');
 
 john.greet();
 jane.greet();
+jane.payTaxes();
 
 $('h1').remove();
